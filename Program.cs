9619aeef9cff8e168bde29b38062be70d3a29516
anassuv <114801727+anassuv@users.noreply.github.com>
2022-11-06 09:13:32 +0300

    z10

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трёхзначное число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 999 || n < 100)
+            {
+                Console.WriteLine("число не трёхзначное");
+            }
+            else
+            {                
+                Console.WriteLine("{0}", (n % 100) / 10);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
